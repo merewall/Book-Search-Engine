f756c350010563e1e112a31a85f77c0906d8e95f
osiyo <merewall@gmail.com>
2021-09-21 03:06:25 -0400

    updating server.js with Apollo server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
+// BRING IN EXPRESS MODULE
 const express = require('express');
+// BRING IN APOLLO SERVER
+const { ApolloServer } = require('apollo-server-express');
+
+// BRING IN PATH MODULE
 const path = require('path');
+
+// BRING IN TYPEDEFS AND RESOLVERS
+const { typeDefs, resolvers } = require('./schemas');
+
+// DATABASE CONNECTION
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+// DO WE NEED THIS!?!?
+// const routes = require('./routes');
+
+// SET PORT AND INSTANCE OF EXPRESS SERVER
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+// CREATE NEW INSTANCE OF APOLLO SERVER PASSING IN TYPEDEFS AND RESOLVERS
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// CONNECT APOLLO SERVER TO EXPRESS
+server.applyMiddleware({ app });
+
+// DATA PARSING & ABILITY TO USE JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +37,18 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// DO WE NEED THIS!?!?
+// app.use(routes);
 
+// WILDCARD ROUTE TO DIRECT TO HOMEPAGE
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// START APP LISTENING
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
