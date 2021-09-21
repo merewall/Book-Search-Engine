db16c2a2fb2572386b09a95ab41882147bd43304
osiyo <merewall@gmail.com>
2021-09-21 11:32:44 -0400

    updating server auth utils

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,25 +1,28 @@
+// BRING IN JSON WEB TOKEN MODULE
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// SET TOKEN SECRET AND EXPIRATION
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+  // FUNCTION FOR OUR AUTHENTICATED ROUTES
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // ALLOWS TOKEN TO BE SENT VIA REQ.BODY, REQ.QUERY OR HEADERS
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
+    // SPLIT TOKEN INTO AN ARRAY AND RETURN ACTUAL TOKEN
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    // IF NO TOKEN, SEND ERROR STATUS AND MESSAGE
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
-    // verify token and get user data out of it
+    // IF TOKEN IS VERIFIED, ADD THE DECODED USER'S DATA TO THE REQUEST SO IT CAN BE ACCESSED BY THE RESOLVER, OTHERWISE SEND ERROR MESSAGE
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -28,12 +31,13 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
+    // SEND TO NEXT ENDPOINT
     next();
   },
+  // PASSING THE USER'S USERNAME, EMAIL AND ID FROM THE LOGIN RESOLVER TO THE SIGNTOKEN FUNCTION
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
+    // USE JSON WEB TOKEN PACKAGE TO "HASH" USER INFO AND GIVE EXPIRATION OF TOKEN
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
