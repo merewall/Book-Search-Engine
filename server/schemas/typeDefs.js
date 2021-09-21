434552d7fa649a8dbed168f3f36e39c9904d6b09
osiyo <merewall@gmail.com>
2021-09-21 10:56:10 -0400

    adding typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    saveBook(userId: ID!, {bookId: String!, authors: [String], description: String, image: String, link: String, title: String!}): User
+    deleteBook(userId: ID!, bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
